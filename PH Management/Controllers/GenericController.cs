6410b61020fbceaef0b35cfffe0af6ee9be26608
Palash Kanti Bachar <palash924332931@gmail.com>
2017-12-02 13:32:42 +0600

    add project

---
+++ PH Management/Controllers/GenericController.cs
@@ -0,0 +1,50 @@
+﻿using PH_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PH_Management.Controllers
+{
+    public class GenericController : Controller
+    {
+        // GET: Generic
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult prGetEmployeeList()
+        {            
+            using (LoanAPPEntities dc = new LoanAPPEntities())
+            {
+                var result = dc.prGetEmployee("").ToList();
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult prGetProductList(string type )
+        {
+            using (LoanAPPEntities dc = new LoanAPPEntities())
+            {
+                var result = dc.prGetEmployee("").ToList();
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult prGetGroupDetails(string type)
+        {
+            using (LoanAPPEntities dc = new LoanAPPEntities())
+            {
+                var result = dc.prGetGroupDetails(type).ToList();
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+    }
+}
---
+++ PH Management/Scripts/masterLib.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+    $(".date-time-picker").datepicker();
+});
+
+var pCalculator = {
+    fnCommonService: function (url, data) {
+        var returnResult;
+        $.ajax({
+            type: "POST",
+            async: false,
+            url: url,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                returnResult = response;
+            },
+            failure: function (response) {
+                alert("failure:" + response.d);
+            },
+            error: function (response) {
+                alert("error:" + response.d);
+            }
+        });
+        return returnResult;
+    },
+    fnCommonDropdown: function (url, data) {
+        var returnResult;
+        $.ajax({
+            type: "POST",
+            async: false,
+            url: url,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                returnResult = response;
+            },
+            failure: function (response) {
+                alert("failure:" + response.d);
+            },
+            error: function (response) {
+                alert("error:" + response.d);
+            }
+        });
+        return returnResult;
+    },
+}
